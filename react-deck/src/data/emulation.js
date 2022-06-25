b0e2fca8ed125c32ed4690f629e9d3fec5b23748
bm <studyhogllc@gmail.com>
2022-06-25 00:15:41 -0400

    #11 - initial attempt at migrating away from gh-pages to react

---
+++ react-deck/src/data/emulation.js
@@ -0,0 +1,10 @@
+export const EMULATION_RESOURCES = [
+    {
+        "title": "Emudeck",
+        "link": "https://github.com/dragoonDorise/EmuDeck"
+    },
+    {
+        "title": "PPSSPP",
+        "link": "https://github.com/hrydgard/ppsspp"
+    },
+]
---
+++ react-deck/src/data/guides.js
@@ -0,0 +1,10 @@
+export const GUIDE_RESOURCES = [
+    {
+        "title": "Emulation Guide",
+        "link": "https://github.com/nchristopher/steamdeck-emulation"
+    },
+    {
+        "title": "Steam Deck Guide",
+        "link": "https://github.com/mikeroyal/Steam-Deck-Guide"
+    },
+]
