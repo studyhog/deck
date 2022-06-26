3808e462fe83587ef6a3e170141131739c770117
bm <studyhogllc@gmail.com>
2022-06-26 18:22:33 -0400

    #8 - game performance & review section added.

---
+++ src/data/game_review.js
@@ -0,0 +1,20 @@
+export const GAME_REVIEW_RESOURCES = [
+  {
+    "title": "ShareDeck",
+    "link": "https://www.sharedeck.games/",
+    "type": "link",
+    "description": "An unofficial site to find and share Steam Deck performance configurations.",
+    "badges": [
+
+    ],
+  },
+  {
+    "title": "Steam Deck HQ",
+    "link": "https://steamdeckhq.com/",
+    "type": "link",
+    "description": "Steam Deck HQ's goal is to provide information on performance of games on the deck and a starting point for configurations with different priorities.",
+    "badges": [
+
+    ],
+  }
+]
