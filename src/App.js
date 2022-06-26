66135ee5ece13c3ee0d5da07c319c1ebf53a7c0e
bm <studyhogllc@gmail.com>
2022-06-26 17:03:05 -0400

    #11 - Additional react scaffolding - favicon and page titles updated. #3 - Added support for descriptions.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import "./App.css";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { Page } from "./components/Page";
-
+import { Header } from "./components/Header";
 import { EMULATION_RESOURCES } from "./data/emulation";
 import { GUIDE_RESOURCES } from "./data/guides";
-
-import "./App.css";
 
 const darkTheme = createTheme({
   palette: {
@@ -12,18 +11,27 @@ const darkTheme = createTheme({
   }
 });
 
+const ALL_RESOURCES = {
+  "Emulation": EMULATION_RESOURCES,
+  "Guide": GUIDE_RESOURCES
+}
+
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
-      <div className="App">
-        <Page
-          title={"Emulation"}
-          resources={EMULATION_RESOURCES}
-        />
-        <Page
-          title={"Guides"}
-          resources={GUIDE_RESOURCES}
-        />
+      <div className="App" style={{
+        background: darkTheme.palette.background.default,
+      }}>
+        <Header title={"Steam Deck Resources"}/>
+        {Object.entries(ALL_RESOURCES).map(([title, resources], i) =>
+          <div key={`${title}-${i}-container-div`}>
+            <Page
+              title={title}
+              resources={resources}
+            />
+            <br/>
+          </div>
+        )}
       </div>
     </ThemeProvider>
   );
