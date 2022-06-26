f845449f7472542f6e5ba28b2979080f1fda18db
bm <studyhogllc@gmail.com>
2022-06-26 17:34:29 -0400

    #11 - other links fixed.

--- src/data/other.js
+++ src/data/other.js
@@ -1,49 +1,29 @@
 export const OTHER_RESOURCES = [
   {
-    "title": "Proton",
-    "link": "https://github.com/ValveSoftware/Proton",
+    "title": "steam-deck-calculator",
+    "link": "https://github.com/MooNag/steam-deck-calculator",
     "type": "repo",
-    "description": "Compatibility tool for Steam Play based on Wine and additional components.",
+    "description": 'Site to estimate order availability for the steam deck. Deployed at https://steam-deck-calculator.web.app',
     "badges": [
       {
         "alt": "GitHub Watchers",
-        "src": "https://img.shields.io/github/watchers/ValveSoftware/Proton?style=social"
+        "src": "https://img.shields.io/github/watchers/MooNag/steam-deck-calculator?style=social"
       },
       {
         "alt": "GitHub Forks",
-        "src": "https://img.shields.io/github/forks/ValveSoftware/Proton?style=social"
+        "src": "https://img.shields.io/github/forks/MooNag/steam-deck-calculator?style=social"
       },
       {
         "alt": "GitHub Stars",
-        "src": "https://img.shields.io/github/stars/ValveSoftware/Proton?style=social"
+        "src": "https://img.shields.io/github/stars/MooNag/steam-deck-calculator?style=social"
       },
     ]
   },
   {
-    "title": "SteamOS",
-    "link": "https://github.com/ValveSoftware/SteamOS",
-    "type": "repo",
-    "description": "SteamOS community tracker.",
-    "badges": [
-      {
-        "alt": "GitHub Watchers",
-        "src": "https://img.shields.io/github/watchers/ValveSoftware/SteamOS?style=social"
-      },
-      {
-        "alt": "GitHub Forks",
-        "src": "https://img.shields.io/github/forks/ValveSoftware/SteamOS?style=social"
-      },
-      {
-        "alt": "GitHub Stars",
-        "src": "https://img.shields.io/github/stars/ValveSoftware/SteamOS?style=social"
-      },
-    ]
-  },
-  {
-    "title": "Steam Deck - Steam News Hub",
-    "link": "https://store.steampowered.com/news/app/1675200",
+    "title": "ProtonDb",
+    "link": "https://www.protondb.com/",
     "type": "link",
-    "description": "Steam News Hub (steam store page for Steam Deck news)",
+    "description": "Home for crowdsourced Linux and Steam Deck game compatibility reports",
     "badges": [
 
     ]
