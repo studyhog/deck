9de00c11abe26d6b1bdb4b001c2df249127e372f
studyhog <57380807+studyhog@users.noreply.github.com>
2023-06-28 08:17:52 -0400

    Correct mis-paste in guide.js
    Haven't slept enough

--- src/data/guide.js
+++ src/data/guide.js
@@ -168,7 +168,7 @@ export const GUIDE_RESOURCES = [
         "link": "https://gist.github.com/james2432/a49f035d0f81c8306df28c1be24a08b1",
         "type": "link",
         "description": "Guide on how to setup SSH on steamdeck with private/public keys",
-        "badges": "badges": [
+        "badges": [
             {
                 "alt": "GitHub Gist Stars",
                 "src": "https://img.shields.io/github/gist/stars/a49f035d0f81c8306df28c1be24a08b1"
