5cb352b6346838a33563c3d72db2cac9ea0d8f8f
John Cyrill Corsanes <4709030+jcchikikomori@users.noreply.github.com>
2025-09-20 21:13:32 +0800

    [refactor] developer tools

---
+++ src/data/devtools.js
@@ -0,0 +1,42 @@
+export const DEVELOPER_TOOLS = [
+  {
+    "title": "distrobox",
+    "link": "https://github.com/89luca89/distrobox",
+    "type": "repo",
+    "description": 'Distrobox is a tool that allows you to easily create and manage containerized environments on your Linux system. It leverages Podman or Docker to create and manage these environments, allowing you to run different Linux distributions in isolated containers.',
+    "badges": [
+      {
+        "alt": "GitHub Watchers",
+        "src": "https://img.shields.io/github/watchers/89luca89/distrobox?style=social"
+      },
+      {
+        "alt": "GitHub Forks",
+        "src": "https://img.shields.io/github/forks/89luca89/distrobox?style=social"
+      },
+      {
+        "alt": "GitHub Stars",
+        "src": "https://img.shields.io/github/stars/89luca89/distrobox?style=social"
+      },
+    ]
+  },
+  {
+    "title": "BoxBuddy",
+    "link": "https://github.com/Dvlv/BoxBuddyRS",
+    "type": "link",
+    "description": 'BoxBuddy is a GUI for Distrobox, making it easier to manage your containerized environments without needing to use the command line.',
+    "badges": [
+      {
+        "alt": "GitHub Watchers",
+        "src": "https://img.shields.io/github/watchers/Dvlv/BoxBuddyRS?style=social"
+      },
+      {
+        "alt": "GitHub Forks",
+        "src": "https://img.shields.io/github/forks/Dvlv/BoxBuddyRS?style=social"
+      },
+      {
+        "alt": "GitHub Stars",
+        "src": "https://img.shields.io/github/stars/Dvlv/BoxBuddyRS?style=social"
+      }
+    ]
+  },
+]
